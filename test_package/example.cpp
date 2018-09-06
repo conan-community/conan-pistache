0e0cae6d13771117b703475fe4e205f40be053d6
Javier G. Sogo <jgsogo@gmail.com>
2018-09-06 17:22:02 +0200

    update with example code

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -12,17 +12,20 @@ public:
     HTTP_PROTOTYPE(HelloHandler)
 
     void onRequest(const Http::Request& request, Http::ResponseWriter response) {
-         response.send(Http::Code::Ok, "Hello, World");
+        UNUSED(request);
+        response.send(Pistache::Http::Code::Ok, "Hello World\n");
     }
 };
 
 int main() {
-    Net::Address addr(Net::Ipv4::any(), Net::Port(9080));
+    Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(9080));
+    auto opts = Pistache::Http::Endpoint::options()
+        .threads(1);
 
-    auto opts = Http::Endpoint::options().threads(1);
     Http::Endpoint server(addr);
     server.init(opts);
-    server.setHandler(std::make_shared<HelloHandler>());
-    // server.serve();
+    server.setHandler(Http::make_handler<HelloHandler>());
+    //server.serve();
+
     server.shutdown();
 }
