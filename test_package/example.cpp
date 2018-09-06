cb3864effc543161b6761dfb7018267ad81b9055
Javier G. Sogo <jgsogo@gmail.com>
2018-09-06 16:59:25 +0200

    forgot to add that code

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -4,6 +4,18 @@
 #include "pistache/endpoint.h"
 
 
+using namespace Net;
+
+class HelloHandler : public Http::Handler {
+public:
+
+    HTTP_PROTOTYPE(HelloHandler)
+
+    void onRequest(const Http::Request& request, Http::ResponseWriter response) {
+         response.send(Http::Code::Ok, "Hello, World");
+    }
+};
+
 int main() {
     Net::Address addr(Net::Ipv4::any(), Net::Port(9080));
 
