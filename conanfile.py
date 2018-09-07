053ac03352f5611eea2bb9a2c85f9f50ba09e8b9
Javier G. Sogo <jgsogo@gmail.com>
2018-09-07 09:57:04 +0000

    requires libstdc++11 to work

--- conanfile.py
+++ conanfile.py
@@ -16,6 +16,11 @@ class PistacheConan(ConanFile):
     def src_folder(self):
         return "{}-{}".format(self.name, self.version)
 
+    def config_options(self):
+        if self.settings.compiler == 'gcc' and float(self.settings.compiler.version.value) >= 5.1:
+            if self.settings.compiler.libcxx != 'libstdc++11':
+                raise ConanException("You must use the setting compiler.libcxx=libstdc++11")
+
     def source(self):
         git = tools.Git(folder=self.src_folder)
         git.clone("https://github.com/oktal/pistache.git", "master")
@@ -23,7 +28,7 @@ class PistacheConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.definitions["PISTACHE_BUILD_TESTS"] = False
-        cmake.definitions["PISTACHE_BUILD_EXAMPLES"] = False
+        cmake.definitions["PISTACHE_BUILD_EXAMPLES"] = True
         cmake.configure(source_folder=self.src_folder)
         cmake.build()
 
@@ -36,7 +41,7 @@ class PistacheConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.includedirs = ['include', ]
-        self.cpp_info.libs = ["pistache"]
+        self.cpp_info.libs = ["pistache", ]
 
         if not self.settings.os == "Windows":
             self.cpp_info.cppflags = ["-pthread"]
